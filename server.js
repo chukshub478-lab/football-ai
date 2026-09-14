c278cb71c195b66cc361f04bedea5c4c0e25eecb
chukshub478-lab <chukshub478@gmail.com>
2026-09-14 22:56:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,14 @@ const cache = new Map();
 
 const CACHE_DURATION =
     10 * 60 * 1000;
-
+function nigeriaDate() {
+    return new Intl.DateTimeFormat("en-CA", {
+        timeZone: "Africa/Lagos",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit"
+    }).format(new Date());
+    }
 
 /*
 ===========================================================
